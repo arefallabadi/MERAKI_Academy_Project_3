b4906573421e5bf76840f77ecd2445eaf9f3a3b7
arefallabadi <labadi.aref@gmail.com>
2021-05-14 16:19:43 +0300

    card 4 done (getArticlesByAuthor)

--- main.js
+++ main.js
@@ -3,48 +3,60 @@ const app = express();
 const port = 5000;
 
 const articles = [
-    {
+  {
     id: 1,
-    title: 'How I learn coding?',
-    description:
-    'Lorem, Quam, mollitia.',
-    author: 'Jouza',
-    },
-    {
+    title: "How I learn coding?",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+  {
     id: 2,
-    title: 'Coding Best Practices',
-    description:
-    'Lorem, ipsum dolor sit, Quam, mollitia.',
-    author: 'Besslan',
-    },
-    {
+    title: "Coding Best Practices",
+    description: "Lorem, ipsum dolor sit, Quam, mollitia.",
+    author: "Besslan",
+  },
+  {
     id: 3,
-    title: 'Debugging',
-    description:
-    'Lorem, Quam, mollitia.',
-    author: 'Jouza',
-    },
-    ];
+    title: "Debugging",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+];
 
-  app.get("/articles",(req,res)=>{
+app.get("/articles", (req, res) => {
+  res.status(200);
+  res.json(articles);
+});
+
+
+app.get("/articles/search_1/:author", (req, res) => {
+  const article = req.params.author;
+  const found = articles.filter((elm) => {
+    return elm.author === article;
+  });
+  if (found.length) {
     res.status(200);
-    res.json(articles);
-  })
+    res.json(found);
+  } else {
+    res.status(404);
+    res.json("article not found");
+  }
+});
 
-  app.get("/articles/:id",(req,res)=>{
-    const article = req.params.id
-    const found = articles.find((elm)=>{
-return elm.id == article 
-    })
-    if(found){
-        res.status(200);
-        res.json(found);
-    }else{
-        res.status(404);
-        res.json("article not found");
-    }  
-  })
+app.get("/articles/:id", (req, res) => {
+    const article = req.params.id;
+    const found = articles.find((elm) => {
+      return elm.id === Number(article);
+    });
+    if (found) {
+      res.status(200);
+      res.json(found);
+    } else {
+      res.status(404);
+      res.json("article not found");
+    }
+  });
 
 app.listen(port, () => {
-    console.log(`server start on ttp://localhost:${port}`);
-  });
+  console.log(`server start on ttp://localhost:${port}`);
+});
