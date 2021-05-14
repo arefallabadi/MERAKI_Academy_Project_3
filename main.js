501675fd4cdf3a2c74147d5488df90543dea053e
arefallabadi <labadi.aref@gmail.com>
2021-05-14 14:13:32 +0300

    card 2 done (getAllArticles)

--- main.js
+++ main.js
@@ -2,6 +2,35 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const articles = [
+    {
+    id: 1,
+    title: 'How I learn coding?',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    {
+    id: 2,
+    title: 'Coding Best Practices',
+    description:
+    'Lorem, ipsum dolor sit, Quam, mollitia.',
+    author: 'Besslan',
+    },
+    {
+    id: 3,
+    title: 'Debugging',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    ];
+
+  app.get("/articles",(req,res)=>{
+    res.status(200);
+    res.json(articles);
+  })
+
 app.listen(port, () => {
     console.log(`server start on ttp://localhost:${port}`);
   });
