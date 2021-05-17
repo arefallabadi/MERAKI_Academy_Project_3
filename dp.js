e68f399ba62b71ddf774020e9ddb02fe7b8b17ed
arefallabadi <labadi.aref@gmail.com>
2021-05-17 11:55:13 +0300

    card 0 (p Starting Steps (MongoDB))

---
+++ dp.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+​
+const option = {
+  useCreateIndex: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+};
+mongoose.connect("mongodb://localhost:27017/project_3_v01", option).then(
+    ()=>{ console.log("DB connected");},
+    (err)=>{console.log(err);}
+)
---
+++ schema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const users = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  age: { type: Number },
+  country: { type: String },
+  email: { type: String },
+  password: { type: String },
+});
+
+const articles = new mongoose.Schema({
+  title: { type: String },
+  description: { type: String },
+  author: { type: mongoose.Schema.ObjectId, ref: "users" },
+});
+
+const User = mongoose.model("user", users);
+const Article = mongoose.model("article", articles);
+module.exports.User = User;
+module.exports.Article =Article ;
